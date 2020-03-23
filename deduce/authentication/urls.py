40f8f79a58785bf8b3136d2b946fc399009ee19e
agzuniverse <aswinganesh666@gmail.com>
2020-03-24 00:16:57 +0530

    Add test endpoints for logout and login redirects

--- deduce/authentication/urls.py
+++ deduce/authentication/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
-from .views import AuthComplete
+from .views import AuthComplete, LogoutSuccess
 
-urlpatterns = ["authcomplete", AuthComplete.as_view()]
+urlpatterns = [
+    # Only for testing purposes, remove this later.
+    # Configure auth0 redirect to the public facing URL of the frontend landing page before deploying
+    "authcomplete",
+    AuthComplete.as_view(),
+    "logoutsuccess",
+    LogoutSuccess.as_view(),
+]
