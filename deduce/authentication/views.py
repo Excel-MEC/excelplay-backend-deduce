9b955503528392ae7818618a0c886f62556984b7
agzuniverse <aswinganesh666@gmail.com>
2020-03-23 23:51:09 +0530

    Add callback endpoint to redirect to on successful login

--- deduce/authentication/views.py
+++ deduce/authentication/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class AuthComplete(APIView):
+    def get(self, req):
+        return Response("Login successful")
