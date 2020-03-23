aeb8d509f386b5d06719d69637c868002a670153
agzuniverse <aswinganesh666@gmail.com>
2020-03-24 01:40:18 +0530

    Create user model

--- deduce/authentication/models.py
+++ deduce/authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.CharField(primary_key=True, max_length=100)
+    name = models.CharField(max_length=200)
+    profile_picture = models.URLField(null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+
+    def __str__(self):
+        return "{0} {1}".format(self.id, self.name)
