07e3be1a509c7ddaa21018238a8c0b7ad8ef778b
agzuniverse <aswinganesh666@gmail.com>
2020-03-24 18:10:09 +0530

    Rename token to signin

--- deduce/authentication/urls.py
+++ deduce/authentication/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import AuthComplete, LogoutSuccess, SignIn, TokenRefresh
 
 urlpatterns = [
-    path("token", SignIn.as_view()),
+    path("signin", SignIn.as_view()),
 ]
